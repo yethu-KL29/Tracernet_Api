000fba8022182bf580fe83416ec184b7b2699477
yethu <yethukichu55@gmail.com>
2023-06-23 10:12:02 +0530

    updated

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
+const multer = require('multer');
+const fs = require('fs');
+const  Image  = require('./models/lostModel');
 
 // Import Routes
 const route = require('./router/userRouter');
@@ -16,6 +19,40 @@ app.use(cors());
 
 app.use('/', route);
 
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./uploads");
+	},
+	filename: (req, file, cb)=> {
+		cb(null, `${(Date.now())}_${file.originalname}`);
+	}
+});
+
+const upload = multer({ storage: storage });
+
+
+app.post("/upload", upload.single("testImage"), async (req, res) => {
+	try {
+	  const saveImage = new Image({
+		name: req.body.name,
+		img: {
+		  data: fs.readFileSync("uploads/" + req.file.filename),
+		  contentType: "image/png",
+		},
+	  });
+  
+	   await saveImage.save();
+	  res.status(200).send('IMAGE SAVED');
+	} catch (err) {
+	  console.log(err);
+	  res.status(500).send("Internal Server Error");
+	}
+  });
+  
+  process.on('unhandledRejection', error => {
+	console.log('unhandledRejection', error.message);
+  });
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
--- models/lostModel.js
+++ models/lostModel.js
@@ -1,46 +1,14 @@
 const mongoose = require('mongoose');
-const multer = require('multer');
 
-// Set up multer for file upload
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
+const imageSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    img: {
+        data: Buffer,
+        contentType: String,
+    },
 });
 
-const upload = multer({ storage: storage });
-
-// Define the user schema
-const lostSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    // required: true
-  },
-  address: {
-    type: String,
-    // required: true
-  },
-  location: {
-    type: String,
-    // required: true
-  },
-  contactNumber: {
-    type: String,
-    // required: true
-  },
-  image: {
-    type: String,
-    required: true
-  }
-});
-
-// Create and export the user model
-const Lost = mongoose.model('Lost', lostSchema);
-
-module.exports = {
-  Lost,
-  upload
-};
+module.exports = new mongoose.model('Image', imageSchema);
